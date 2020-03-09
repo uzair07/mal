6900cb8f7bfa9982f642c9070fbfd3cdaaa024e7
uzair07 <50266872+uzair07@users.noreply.github.com>
2020-03-09 15:20:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,request
+import malware_test
+app = Flask(__name__)  
+ 
+@app.route('/')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename) 
+        s = malware_test.try1(f.filename)
+        return render_template("success.html",name='The file %s is %s' % (f.filename,['malicious', 'legitimate'][s]))
+  
+if __name__ == '__main__':  
+    app.run(debug = True)  
